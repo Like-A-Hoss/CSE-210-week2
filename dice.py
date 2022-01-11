00ea4ba78defc7c130e0c83d53a1b27d7c921fcc
Like-A-Hoss <78040022+Like-A-Hoss@users.noreply.github.com>
2022-01-11 14:46:13 -0600

    Updated Dice and Player
    Updated the dice and player classes.  just need to set up main to handle the game part of the game.

--- dice.py
+++ dice.py
@@ -18,9 +18,21 @@ class dice():
     def __init__(self) -> None:
         self.rolls =[]
     def set_rolls(self):
+        """Rolls 5d6 and puts resulsts in list"""
         counter =1
         while counter <=5:
             d6 = die()
             d6.roll()
             self.rolls.append(d6.see_roll())
-        
+    def get_rolls(self):
+        """displays results of rolls as a print command"""
+        print(f"[{self.rolls[0]}] [{self.rolls[1]}] [{self.rolls[2]}] [{self.rolls[3]}] [{self.rolls[4]}]")
+
+class player():
+    def __init__(self) -> None:
+        """sets up a player"""
+        self.player_name = "Player 1"
+        self.score = 0
+    def set_score(self, points):
+        """sets the score by adding the points passed in."""
+        self.score += points
